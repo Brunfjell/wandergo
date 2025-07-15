135d2a89126c1c67e293c967c45a3d531662fe69
Brunfjell <luistroyparaiso@gmail.com>
2025-07-15 10:50:14 +0800

    v1.2.2 fix2

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyCA5C65XVI5ifwlMwFpldLdKPb2-ygbwHk",
+  authDomain: "wandergo-carrental.firebaseapp.com",
+  projectId: "wandergo-carrental",
+  storageBucket: "wandergo-carrental.firebasestorage.app",
+  messagingSenderId: "623055405824",
+  appId: "1:623055405824:web:f8055d3780d268cefda9e5",
+  measurementId: "G-WFJTEYP5X1"
 };
 
 // Initialize Firebase
